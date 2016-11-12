1586ab7ec9540ca1e502a4f4445c1309966d075d
wojtekw92 <wwozniak92@gmail.com>
2016-11-12 15:11:51 +0100

    action support for gameLoop

--- game.js
+++ game.js
@@ -12,13 +12,14 @@ var Game = function() {
 Game.prototype.gameLoop = function(action) {
   if (this.deck.length+this.activeDeck.length) {
     if (action.type === "finish") {
-      this.activePlayerFinishGame();
+      this.activePlayerFinishGame(action);
     } else if (action.type === "buy") {
 
     }
 
 
     this.activePlayer++;
+    //cbk updateDecks
     if (this.activePlayer === this.players.length) {
       this.activePlayer = 0;
       //round end (make some actions from kartaKurwaWioski)
@@ -31,11 +32,12 @@ Game.prototype.gameLoop = function(action) {
 Game.prototype.init = function () {
 }
 
-Game.prototype.activePlayerFinishGame = function () {
+Game.prototype.activePlayerFinishGame = function (action) {
   if (this.activeDeck.length) {
     this.trash.push(this.activeDeck[0]);
   }
-
+  this.players[this.activePlayer].setCoins(action.coinStatus);
+  this.players[this.activePlayer].setHealth(action.healthStaus);
   this.activeDeck = shift(this.activeDeck);
   this.activeDeck.push(this.deck.pop());
 }
