a3d4a9823e626c001e76d5443b83b4713fe39c53
Maciej Małecki <me@mmalecki.com>
2016-11-12 12:22:54 +0100

    Start working on our routing

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 
 const http = require('http')
 const ws = require('ws')
+const Game = require('./game.js')
 
 const PACKAGE = require('./package.json')
 const SERVER = PACKAGE.name + '/' + PACKAGE.version
+
+var games = {}
 
 const server = http.createServer((req, res) => {
   res.setHeader('server', SERVER)
@@ -16,8 +19,19 @@ const wsServer = new ws.Server({ server: server })
 wsServer.on('connection', s => {
   s.on('message', msg => {
     const parsed = JSON.parse(msg)
-    const game = parsed.topic
+    const room = parsed.topic
+    const event = parsed.event
 
+    var game
+
+    // Whatever you say goes, boss.
+    if (games[room]) game = games[room]
+    else game = games[room] = new Game()
+
+    if (event === 'start') {
+      game.init()
+
+    }
   })
 })
 
