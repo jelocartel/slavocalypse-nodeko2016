a284600eb96639feeb6b2012da4c9a6308953b3f
Michał Budzyński <michal@virtualdesign.pl>
2016-11-13 00:21:34 +0100

    stone skills

---
+++ cards/stone.js
@@ -0,0 +1,27 @@
+const deck = 'earth';
+
+const skills = [{
+  name: 'Hands of God rune',
+  victoryPoints: 1,
+  description: 'At the en of the turn chose player that -$1',
+  onact: function (game, players) {
+  },
+  onbuy: function (game, players) {
+  }
+}, {
+  name: 'Shield of Perun rune',
+  description: 'Trash this for +5 Attack agains Deities',
+  onact: function (game, players) {
+  },
+  onbuy: function (game, players) {
+  }
+}, {
+  name: 'Perun\'s Thunder rune',
+  description: 'When gained, +6 wounds',
+  onact: function (game, players) {
+  },
+  onbuy: function (game, players) {
+  }
+}];
+
+module.exports.skills = skills;
