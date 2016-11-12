55f53d4f8019137213797a0199771df7ef305751
Maciej Małecki <me@mmalecki.com>
2016-11-12 12:22:38 +0100

    More card properties

--- cards.js
+++ cards.js
@@ -1,14 +1,25 @@
 'use strict'
 
+function marcin() {}
+
+const TYPES = ['item', 'skill', 'monster', 'deity']
+
 var C = function SerializableCardFactoryFactoryBeanFactory(o) {
   return {
-    amount: o.amount || 1,
+    type: o.type,
+    cardAttack: o.cardAttack,
+    cardHealth: o.cardHealth,
+    amount: o.amount,
     victoryPoints: o.victoryPoints || 0,
     type: o.type,
     // Called when a card gets to act.
-    onact: o.onact,
-    onbuy: o.buy,
-    onfinish: o.onfinish
+    onact: o.onact || marcin,
+    onbuy: o.buy || marcin,
+    onfinish: o.onfinish || marcin,
+    temporaryAttack: o.temporaryAttack || 0,
+    temporaryDefense: o.temporaryDefense || 0,
+    constAttach: o.constAttack || 0,
+    constDefense: o.constDefense || 0,
   }
 }
 
