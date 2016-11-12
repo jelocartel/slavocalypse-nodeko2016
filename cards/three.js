7517e115c55d1a8d4ee7bcbe3ece414cb8cbd227
Michał Budzyński <michal@virtualdesign.pl>
2016-11-13 00:30:17 +0100

    three skill names

--- cards/three.js
+++ cards/three.js
@@ -1,7 +1,7 @@
 const deck = 'three';
 
 const skills = [{
-  name: 'Hands of War God',
+  name: 'Three of Gods',
   victoryPoints: 1,
   description: 'When gaining a monster, -1 wound',
   onact: function (game, players) {
@@ -9,14 +9,14 @@ const skills = [{
   onbuy: function (game, players) {
   }
 }, {
-  name: 'War Thunder',
+  name: 'Grass Thunder',
   description: 'At the end of turn choose a player for -1 wound, then -2 wound for you',
   onact: function (game, players) {
   },
   onbuy: function (game, players) {
   }
 }, {
-  name: 'Shield of Veles',
+  name: 'Triskelion',
   description: 'Change $1 for +1 Attack +1 Defense',
   onact: function (game, players) {
   },
