0b46e283a9280d8be9e5300c8c3194945da94e2e
Maciej Małecki <me@mmalecki.com>
2016-11-12 16:50:19 +0100

    make discovery work

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ function roomcast(room, msg) {
   sockets[room].forEach(s => s.send(JSON.stringify(msg)))
 }
 
+function sendState(room) {
+  roomcast(room, {
+    event: 'state',
+    activeDeck: games[room].activeDeck.map(serializeCard),
+    camp: games[room].campCard
+  })
+}
+
+
 function serializeCard(card) {
   return card
 }
@@ -31,8 +40,12 @@ var id = 0
 wsServer.on('connection', s => {
   s.on('message', msg => {
     const parsed = JSON.parse(msg)
-    const room = parsed.topic
+    const room = parsed.game
     const event = parsed.event
+
+    if (event === 'discover') {
+      s.send(JSON.stringify({ event: 'discover', games: Object.keys(games) }))
+    }
 
     var game
 
@@ -43,29 +56,31 @@ wsServer.on('connection', s => {
     }
     else {
       game = games[room] = new Game()
+      game.hooks.onTurnFinish = () => sendState(room)
+      game.hooks.onRoundFinish = () => sendState(room)
+      game.hooks.onGameFinish = () => sendState(room)
       sockets[room] = [ s ]
     }
 
     if (event === 'start') {
       game.start()
-      roomcast({
-        event: 'start',
-        activeDeck: game.activeDeck.map(serializeCard),
-        camp: game.kartaKurwaWioski
+      roomcast(room, {
+        event: 'start'
       })
+      sendState(room)
     }
     else if (event === 'join') {
       const user = new User({ id: id++ })
       game.addUser(user)
-    }
-    else if (event === 'discover') {
-      s.send({ event: 'discover', rooms: Object.keys(rooms) })
+      roomcast(room, { event: 'new-player' })
     }
     else if (event === 'buy') {
       game.gameLoop({ type: 'buy' })
+      sendState(room)
     }
     else if (event === 'finish') {
       game.gameLoop({ type: 'finish', campCardActionId: parsed.campCardActionId })
+      sendState(room)
     }
   })
 })
