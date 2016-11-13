9e00892bf865e5671f5e759c098f7b0d7dac198b
Kuba Wasiak <qbawasiak@wp.pl>
2016-11-13 22:48:24 +0100

    card caounter

--- public/app/network.js
+++ public/app/network.js
@@ -13,6 +13,7 @@ define(['knockout'], function(ko) {
   var enemyPlayers = ko.observableArray([]);
   var activePlayer = ko.observable();
   var player = ko.observable({});
+  var cardsLeft = ko.observable();
 
   var socket = new WebSocket(url);
   var gameStarted = ko.observable(false);
@@ -102,6 +103,7 @@ define(['knockout'], function(ko) {
         playerDecks(parsedEvent.players[playerID()].deck);
         activePlayer(parsedEvent.activePlayer);
         player(parsedEvent.players[playerID()]);
+        cardsLeft(parsedEvent.deckCardsLeft);
         console.log('STATE PLAYER', player());
         break;
       case 'start':
@@ -132,6 +134,7 @@ define(['knockout'], function(ko) {
     socket: socket,
     playerID: playerID,
     activePlayer: activePlayer,
-    player: player
+    player: player,
+    cardsLeft: cardsLeft
   };
 });
